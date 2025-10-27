c73a2c90efc94b2c1ba5bbc5e61ed3dad49b150d
JazzY6113 <kbogomolov22@gmail.com>
2025-10-27 23:36:48 +0700

    add yandex map

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -13,8 +13,10 @@
         </div>
         <div class="contacts">
             <div class="first-line">
-                <div>
-                    <img src="img/geoposition.svg" alt="geo">
+                <div style="position:relative;overflow:hidden;">
+                    <a href="https://yandex.ru/maps/67/tomsk/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:0px;">Томск</a>
+                    <a href="https://yandex.ru/maps/67/tomsk/house/ulitsa_yelizarovykh_18/bE0Yfw9iTUYGQFtsfXh0eH1mYA==/?ll=84.982441%2C56.459021&utm_medium=mapframe&utm_source=maps&z=20.47" style="color:#eee;font-size:12px;position:absolute;top:14px;">Улица Елизаровых, 18 — Яндекс Карты</a>
+                    <iframe src="https://yandex.ru/map-widget/v1/?ll=84.982441%2C56.459021&mode=whatshere&whatshere%5Bpoint%5D=84.982297%2C56.459117&whatshere%5Bzoom%5D=17&z=20.47" width="700" height="500" frameborder="1" allowfullscreen="true" style="position:relative;"></iframe>
                 </div>
             </div>
             <div class="last-line">
@@ -37,7 +39,7 @@
                 <div>
                     <p>
                         Контакты филлиала в Томске:<br>
-                        Томск, ул. Елизаровых, 16<br>
+                        Томск, ул. Елизаровых, 18<br>
                         Пн-Пт: 10:00 - 19:00<br>
                         Сб, Вс: Закрыто
                     </p>
